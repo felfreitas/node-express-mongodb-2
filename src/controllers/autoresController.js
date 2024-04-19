d9263e1882dab92c69ade183fb76cb46ef1c9aac
felfreitas <soufreitas.felipe@gmail.com>
2024-04-19 15:29:13 -0300

    inserindo rota 404

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,3 +1,4 @@
+import NaoEncontrado from "../erros/NaoEncontrado.js";
 import autores from "../models/Autor.js";
 
 class AutorController {
@@ -10,7 +11,7 @@ class AutorController {
 
         } catch (erro) {
             next(erro);
-           
+
         }
     };
 
@@ -29,7 +30,7 @@ class AutorController {
 
             } else {
 
-                res.status(404).send({ message: "Id do Autor não localizado." });
+                next(new NaoEncontrado("Id do Autor não localizado."));
 
             }
 
@@ -47,7 +48,7 @@ class AutorController {
             const autorResultado = await autor.save();
 
             res.status(201).send(autorResultado.toJSON());
-            
+
         } catch (erro) {
             next(erro);
         }
@@ -58,9 +59,15 @@ class AutorController {
         try {
             const id = req.params.id;
 
-            await autores.findByIdAndUpdate(id, { $set: req.body });
+            const autorModificado= await autores.findByIdAndUpdate(id, { $set: req.body });
 
-            res.status(200).send({ message: "Autor atualizado com sucesso" });
+            if(autorModificado !==null){
+
+                res.status(200).send({ message: "Autor atualizado com sucesso" });
+            }else{
+                next(new NaoEncontrado("Autor não localizado."));
+            }
+
         } catch (erro) {
             next(erro);
         }
@@ -70,9 +77,16 @@ class AutorController {
         try {
             const id = req.params.id;
 
-            await autores.findByIdAndDelete(id);
+            const autorDeletado = await autores.findByIdAndDelete(id);
 
-            res.status(200).send({ message: "Autor removido com sucesso" });
+            if (autorDeletado !== null) {
+
+                res.status(200).send({ message: "Autor removido com sucesso" });
+
+            } else {
+                next(new NaoEncontrado("Autor não localizado."));
+            }
+
         } catch (erro) {
             next(erro);
         }
